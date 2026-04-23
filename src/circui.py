4722ffae272d5c0868feb328db5f4f86ed2b3a8e
Anurag Singh <sagacious112358@gmail.com>
2026-04-23 02:38:34 +0200

    Refactor circuit update and stringbit function

--- src/circui.py
+++ src/circui.py
@@ -4,27 +4,42 @@ from qiskit import QuantumCircuit, transpile
 from qiskit_aer import Aer
 from qiskit.quantum_info import Operator
 import numpy as np
+import math as mt
 from functools import reduce
 
 # Function to generate the circuit plot
+def stringbit(qc,s):
+    #print(len(s))
+    for i in range(len(s)-1,-1,-1):
+        #print("i is ",i)
+        if(s[i] == "1"):
+            qc.x(len(s) -1 - i )
+gli = ""
 def update_circuit(angle):
+    global gli
     plt.close('all')  # Clear previous figures to save memory
     
     # 1. Create the Circuit
     qc = QuantumCircuit(5,3)
-    qc.ry(angle, 1)  # RY gate controlled by the slider
-    qc.x(1)
-    qc.x(0)
+    #qc.ry(angle*mt.pi/10, 1)  # RY gate controlled by the slider
+    #qc.x(1)
+    stbi = ["000" ,"01" ,"10" ,"11" ]
+    stringbit(qc,stbi[angle])
     #qc.ccx(2,1, 0)
-    qc.cx(1,0)
-    qc.cx(2,1)
-    qc.cx(2,0)
+    #qc.cx(1,0)
+    #qc.cx(0,1)
+    #qc.x(0)
+    #qc.cx(2,1)
+    #qc.cx(2,0)
+    #qc.cx(3,0)
 
     sp = Operator(qc)
     #print(sp.data.real)
     qc.measure([0],[0])
     qc.measure([1],[1])
     qc.measure([2],[2])
+
+    #qc.measure_all()
 
 
     b0 = np.array([[1,0.0]])
@@ -48,7 +63,8 @@ def update_circuit(angle):
     
     # 3. Update the Text Output
     sll = str(cc.data.T.real)
-    text_output.content = f'```\n\n{sll+"\n==================\n"+str(qc.draw(output="text")) + "\n\n"+str(counts)}\n```'
+    gli = sll+"\n==================\n"+str(qc.draw(output="text")) + "\n\n"+str(counts)+"\n"+stbi[angle]+"\n"
+    text_output.content = f'```\n\n{gli}\n```'
 
 # --- UI Layout ---
 
@@ -57,7 +73,7 @@ ui.label('Quantum Circuit Controller').classes('text-h4 q-ma-md')
 with ui.card().classes('w-full q-pa-md'):
     ui.label('Adjust RY Gate Angle (θ)')
     # Slider from 0 to 2π
-    slider = ui.slider(min=0, max=6.28, step=0.01, value=1.57, on_change=lambda e: update_circuit(e.value))
+    slider = ui.slider(min=0, max=10, step=1, value=0, on_change=lambda e: update_circuit(e.value))
     ui.label().bind_text_from(slider, 'value', backward=lambda v: f'Angle: {v:.2f} rad')
 
 with ui.tabs().classes('w-full') as tabs:
@@ -68,6 +84,6 @@ with ui.tab_panels(tabs, value='Text Output').classes('w-full'):
         text_output = ui.markdown()
 
 # Initialize the display
-update_circuit(1.57)
+update_circuit(0)
 
 ui.run()
